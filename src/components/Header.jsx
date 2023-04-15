1f0dec6f8f57015db0d245e5ae11c436eec55b02
Harsh-0986 <harsh.r.shah2102@gmail.com>
2023-04-15 16:12:08 +0530

    Added share functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export const Header = () => {
   return (
     <div className="w-full flex items-center justify-center lg:grid lg:grid-cols-3 h-1/5">
-      <div className="hidden lg:inline-block text-md m-auto p-4 rounded-full text-black bg-white hover:text-white hover:bg-gray-700 cursor-pointer">
-        <span className="material-icons ">ios_share</span>
-      </div>
+      <div></div>
       <div className="text-md lg:text-xl m-auto p-4 lg:p-8 rounded-full">
         <img
           src={"https://media.graphassets.com/Zh6xzCOnQmCxpXFeRaPj"}
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,41 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import linksData from "../links.js";
 import DynamicIcon from "./DynamicIcon";
+import ShareModal from "./ShareModal";
 
 export const Links = () => {
+  const [open, setOpen] = useState(false);
+  const [url, setUrl] = useState("");
+
   return (
-    <div className="grid grid-row-1 w-8/12 gap-y-2 text-white">
-      {linksData.map((value, index) => {
-        return (
-          <div
-            key={index}
-            className={
-              "h-12 text-white w-full flex justify-around items-center bg-[#222] rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 cursor-pointer"
-            }
-          >
-            <span
-              className={"w-1/5 text-center flex justify-center items-center"}
+    <>
+      <ShareModal
+        url={url}
+        show={open}
+        onClose={() => setOpen(!open)}
+        title="Subscribe to our newsletter"
+      />
+      <div className="grid grid-row-1 w-8/12 gap-y-2 text-white">
+        {linksData.map((value, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                "h-12 text-white w-full flex justify-around items-center bg-[#222] rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 cursor-pointer"
+              }
             >
-              <DynamicIcon icon={value.icon} />
-            </span>
-            <a
-              target={"_blank"}
-              href={value.link}
-              rel="noreferrer"
-              className={"flex w-3/5 justify-center items-center"}
-            >
-              <span className={"w-3/5 text-center hover:text-gray-400"}>
-                {value.title}
+              <span
+                className={"w-1/5 text-center flex justify-center items-center"}
+              >
+                <DynamicIcon icon={value.icon} />
               </span>
-            </a>
-            <span className={"flex w-1/5 items-center justify-center"}>
-              <span className="material-icons w-1/5 hover:text-gray-400 ">
-                ios_share
+              <a
+                target={"_blank"}
+                href={value.link}
+                rel="noreferrer"
+                className={"flex w-3/5 justify-center items-center"}
+              >
+                <span className={"w-3/5 text-center hover:text-gray-400"}>
+                  {value.title}
+                </span>
+              </a>
+              <span
+                className={"flex w-1/5 items-center justify-center"}
+                onClick={() => {
+                  setOpen(!open);
+                  setUrl(value.link);
+                }}
+              >
+                <span className="material-icons w-1/5 hover:text-gray-400">
+                  ios_share
+                </span>
               </span>
-            </span>
-          </div>
-        );
-      })}
-    </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
