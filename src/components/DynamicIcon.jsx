a9e13ba86d25a1bc4a565331e4f964ce7aa25cef
Harsh-0986 <harsh.r.shah2102@gmail.com>
2023-06-12 15:19:49 +0530

    Update DynamicIcon.jsx

--- src/components/DynamicIcon.jsx
+++ src/components/DynamicIcon.jsx
@@ -1,9 +1,11 @@
 import * as bsIcons from "react-icons/bs";
+import * as giIcons from 'react-icons/gi'
 
 const DynamicIcon = ({ icon, className }) => {
   const getIcon = (iconName) => {
     const iconsMap = new Map();
     iconsMap.set("Bs", bsIcons);
+    iconsMap.set("Gi", giIcons);
 
     return iconsMap.get(iconName.substring(0, 2));
   };
