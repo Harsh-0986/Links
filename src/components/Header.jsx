835488a568db61002392e747ff8a13b632778403
Harsh-0986 <harsh2102.dev@gmail.com>
2024-01-27 17:13:44 +0530

    Update some links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ export const Header = () => {
   return (
     <div className="w-full flex items-center justify-center lg:grid lg:grid-cols-3 h-1/5">
       <div></div>
-      <div className="text-md lg:text-xl m-auto p-4 lg:p-8 rounded-full">
+      <div className="overflow-hidden text-md lg:text-xl m-auto p-4 lg:p-8 rounded-full">
         <img
           src={"/favicon.webp"}
-          className={" w-32 lg:w-48 h-32 lg:h-48"}
+          className={"rounded-full w-32 lg:w-48 h-32 lg:h-48"}
           alt={"Harsh Shah"}
         />
         <span className="block w-full text-center mt-2">@Harsh Shah</span>
